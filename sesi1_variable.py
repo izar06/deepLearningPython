73c1121da7839241dfce007cafefd84ede0119b8
izar06 <izar.anam22@gmail.com>
2025-07-13 21:24:21 +0700

    Studying Variables on Python

---
+++ sesi1_variable.py
@@ -0,0 +1,27 @@
+#Variable merupakan tempat untuk menyimpan data
+
+#Menaruh / assignment nilai
+a = 10
+x = 5
+length = 1000
+
+#Pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai length = ", length)
+
+#Penamaan
+#tidak boleh ada spasi di penamaan variable
+#tidak boleh ada angka di depan nama variable
+nilai_y = 15 #Dengan menggunakan underscore
+juta10 = 1000000 #ini boleh
+nilaiX = 17.9 #ini boleh
+
+#Pemanggilan kedua
+print("Nilai a = ", a)
+a = 7 
+print("Nilai a = ", a)
+
+#assignment indirect
+b = a
+print("Nilai b = ", b)
